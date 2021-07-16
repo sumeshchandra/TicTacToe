aa5b2be38930fb6e27f20d0eba0cd5cf7252e4c5
Sumesh chandra jena <sumeshjena.co@gmail.com>
2021-07-16 09:35:35 +0530

    Added: Board Created

---
+++ src/TicTacToeGame.java
@@ -0,0 +1,20 @@
+
+public class TicTacToeGame {
+
+	public char board[] = new char[10]; 
+
+	TicTacToeGame() {
+		creatboard();
+	}
+	// creating creatboard method
+	public void creatboard() {
+	for (int no =1; no < 10; no++) {
+		board[no] = ' ';
+		}	
+	}
+	public static void main(String[] args) {
+//		TicTacToeGame tictactoegame = new TicTacToeGame();
+
+	}
+
+}
